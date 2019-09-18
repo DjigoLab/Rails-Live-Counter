149725e9803bde637a12b0652cf38880e4a102f1
Djigo <antonio.djinav@gmail.com>
2019-09-18 20:07:43 +0100

    Updating migration values for room

--- db/migrate/20190830135545_create_rooms.rb
+++ db/migrate/20190830135545_create_rooms.rb
@@ -1,12 +1,11 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string :name
+      t.string :name, :default => 'Boring Default Name'
       t.string :password, :default => 'empty'
-      t.integer :max_counter, :default => 0
+      t.integer :max_counter, :default => 100
       t.integer :counter_value, :default => 0
-      t.boolean :accepts_negative, :default => false
-
+      t.boolean :accepts_negative
       t.timestamps
     end
   end
