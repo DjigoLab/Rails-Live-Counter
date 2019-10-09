8fa3598c5461af54b0560325238235baad500c0a
Antonio Djigo <antonio@secret-source.eu>
2019-10-09 13:30:09 +0100

    Fixing acceptneg checkbox, minor css fixes

--- app/assets/javascripts/action.js
+++ app/assets/javascripts/action.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 
-    $(".accepts-negatives").click(function() {
+    $(".accepts-negatives").find('label').click(function() {
         !$('#room_accepts_negative').prop('checked') ?
             $('#room_accepts_negative').prop('checked', true) :
             $('#room_accepts_negative').prop('checked', false);
