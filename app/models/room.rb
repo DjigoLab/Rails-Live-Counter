f2a49b148267e091bde92143ad3a93e541e2718c
Antonio Djigo <antonio@secret-source.eu>
2019-10-09 13:21:47 +0100

    Unique names at room creation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
     validates :name,
         presence: true,
-        length: { minimum: 4, maximum: 12 }
+        length: { minimum: 4, maximum: 12 },
+        uniqueness: true
 end
