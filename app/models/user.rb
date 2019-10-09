36446e813e40cb71d11dc677ff7ceaa488b72b02
Antonio Djigo <antonio@secret-source.eu>
2019-10-09 16:07:31 +0100

    Repeatable user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
     validates :name,
         presence: true,
-        length: { maximum: 12 },
-        uniqueness: { case_sensitive: false }
+        length: { maximum: 12 }
+
 end
