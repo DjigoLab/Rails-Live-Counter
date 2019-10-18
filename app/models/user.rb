1e30e0fd93132cc63d6771525c008fc83e320622
Antonio Djigo <antonio@secret-source.eu>
2019-10-18 15:14:17 +0100

    Changing error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     validates :name,
-        presence: true,
+        presence: {message: "You need to type in a username before you can proceed!"},
         length: { maximum: 12 }
 
 end
