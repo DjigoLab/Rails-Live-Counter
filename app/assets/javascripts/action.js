ffd92db1542358e36fff80f54635910c8eeaf541
Antonio <antonio@secret-source.eu>
2019-11-14 16:31:54 +0000

    Design changes

--- app/assets/javascripts/action.js
+++ app/assets/javascripts/action.js
@@ -1,9 +1,12 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-    $(".accepts-negatives").find('label').click(function() {
+    $(".accepts-negatives").find('label').click(function () {
         !$('#room_accepts_negative').prop('checked') ?
             $('#room_accepts_negative').prop('checked', true) :
             $('#room_accepts_negative').prop('checked', false);
     })
 
+    $("#show-list").click(function () {
+        $("#users-list").addClass('show')
+    })
 })
