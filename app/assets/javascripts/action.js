c777ffc2a0561bb95a2118b8ee47371539953ff2
Antonio <antonio@secret-source.eu>
2019-11-15 12:27:11 +0000

    Adding new checkbox from design

--- app/assets/javascripts/action.js
+++ app/assets/javascripts/action.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-    $(".accepts-negatives").find('label').click(function () {
+    $(".accepts-negatives").click(function () {
         !$('#room_accepts_negative').prop('checked') ?
             $('#room_accepts_negative').prop('checked', true) :
             $('#room_accepts_negative').prop('checked', false);
